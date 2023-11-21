74ac6eaa6e34feac18019eb06b20979b10dd5e6d
Ihar Anikeyenka <ianikeyenka@productiveedge.com>
2023-11-21 22:48:44 +0100

    Chats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>Home page</div>
+import CurrentUser from '@/components/screens/(chats)/CurrentUser'
+import ChatsList from '@/components/screens/(chats)/ChatsList'
+import Chat from '@/components/screens/(chats)/Chat'
+
+export default function ChatsPage() {
+  return (
+    <div className="grid" style={{ gridTemplateColumns: '.7fr 3fr' }}>
+      <div className="border-r border-border">
+        <CurrentUser />
+        <ChatsList />
+      </div>
+      <div>
+        <Chat />
+      </div>
+    </div>
+  )
 }
